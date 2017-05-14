dffa3c64b0fe508a7424ffdab252410072121ec4
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-14 14:59:37 -0400

    Make 3 colored boxes

---
+++ app.js
@@ -0,0 +1,16 @@
+//Application State
+const appState = {
+  boxes = [
+
+  ]
+}
+
+
+const boxLayout = {
+  return `<div class="blue_box blue_color">
+        <div class="green_box green_color">
+          <div class="red_box red_color">
+          </div>
+        </div>
+      </div>`;
+}
